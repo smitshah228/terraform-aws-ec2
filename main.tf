d44231a4a180e0fc1347d4a25ee8805d2ba50e72
Smit Shah <smitshah@0e:90:ed:ac:b0:86.home>
2025-04-26 18:14:11 -0400

    Updated Terraform config: added S3 bucket resource and outputs

--- main.tf
+++ main.tf
@@ -11,3 +11,15 @@ resource "aws_instance" "example" {
     Environment = var.environment
   }
 }
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "terraform-smit-bucket-${random_id.bucket_id.hex}" # Must be globally unique
+
+  tags = {
+    Name        = "TerraformS3Bucket"
+    Environment = "Development"
+  }
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
