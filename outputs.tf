3bb7bd2dec4d109d1a73fb110b301e6a3a801c4c
Smit Shah <smitshah@0e:90:ed:ac:b0:86.home>
2025-04-26 18:13:06 -0400

    Added S3 bucket output variables

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
 output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
+  description = "Public IP address of the EC2 instance"
   value       = aws_instance.example.public_ip
 }
 
 output "instance_id" {
-  description = "EC2 Instance ID"
+  description = "ID of the EC2 instance"
   value       = aws_instance.example.id
+}
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket created"
+  value       = aws_s3_bucket.example_bucket.bucket
 }
